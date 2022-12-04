788715157b60197226fc0c4194d903026423e65c
aissam-ahbar <33417377+aissam-ahbar@users.noreply.github.com>
2022-12-04 16:01:02 +0100

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  // Create your collection in your typescript file
   public languages = [
     {
       name: 'Angular',
